23477bf2c00030962341452f729ec5337e2ecc45
Ben Brinckerhoff <ben@bbrinck.com>
2016-11-01 20:23:03 -0600

    Adds wiremock for testing

---
+++ src/flog_experiments/mock_server.clj
@@ -0,0 +1,10 @@
+(ns flog-experiments.mock-server
+  (:require [flog-experiments.wiremock :as wm])
+  )
+
+
+(comment
+  (def wiremock-server (wm/server))
+
+  )
+
--- test/flog_experiments/core_test.clj
+++ test/flog_experiments/core_test.clj
@@ -1,7 +1,20 @@
 (ns flog-experiments.core-test
   (:require [clojure.test :refer :all]
-            [flog-experiments.core :refer :all]))
+            [flog-experiments.core :refer :all]
+            [clj-http.client :as http]
+            [flog-experiments.wiremock :as wm]
+            ))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn wiremock [f]
+  (let [s (wm/server)]
+    (wm/start s)
+    (f)
+    (wm/stop s)))
+
+(use-fixtures :each wiremock)
+
+(deftest mock-content
+  (wm/stub { :request { :method "GET" :url "/hello"}
+            :response { :status 200 :body "Hello World"}})
+  (is (= 200 (:status (http/get "http://localhost:8080/hello"))))
+  (is (= "Hello World" (:body (http/get "http://localhost:8080/hello")))))
